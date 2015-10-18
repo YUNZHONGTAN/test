ad372c93371dd209f0634bd8a91d9d64e055560d
YUNZHONGTAN <781312537@qq.com>
2015-10-18 08:12:11 -0700

    create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_world.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/18/2015 08:10:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yunzhongtan (), 
+ *        Company:  hello
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+    printf("hello world !!!\n");
+
+    exit(0);
+}
